45ede13621f1519aaf6ac9aa72a95b21db0a8ef7
electric-mike <66960109+electric-mike@users.noreply.github.com>
2025-02-13 14:07:20 -0500

    pup stealth mode

--- server/scrape/browser.ts
+++ server/scrape/browser.ts
@@ -1,9 +1,11 @@
-import puppeteer from 'puppeteer'
+import puppeteer from 'puppeteer-extra'
+import StealthPlugin from 'puppeteer-extra-plugin-stealth'
 
 let browser
 
 export async function getBrowser() {
   if (!browser) {
+    puppeteer.use(StealthPlugin())
     browser = await puppeteer.launch({
       headless: true,
       args: ['--no-sandbox', '--disable-setuid-sandbox', '--disable-dev-shm-usage'],
