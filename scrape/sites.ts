6dc585cca6760d36632b821a388468d8b871134f
electric-mike <66960109+electric-mike@users.noreply.github.com>
2025-02-12 20:15:45 -0500

    wip big red

--- scrape/sites.ts
+++ scrape/sites.ts
@@ -2,8 +2,18 @@ import scrapeBinnys from './scrapeBinnys'
 
 export default [
   {
-    name: 'FOD Small Batch - Binnys',
+    name: 'Small Batch - Binnys',
     url: 'https://www.binnys.com/spirits/whiskey/field-of-dreams-small-batch-bourbon-160342/',
     scraper: scrapeBinnys,
   },
+  // {
+  //   name: 'Small Batch - Big Red',
+  //   url: 'https://bigredliquors.com/shop/product/field-of-dreams-small-batch-bourbon-whiskey/670fdbe1d76d41428066111b',
+  //   scraper: scrapeBigRed,
+  // },
+  // {
+  //   name: 'Players Series - Big Red',
+  //   url: 'https://bigredliquors.com/shop/product/field-of-dreams-small-batch-bourbon-whiskey/670fdbe1d76d41428066111b',
+  //   scraper: scrapeBigRed,
+  // },
 ]
