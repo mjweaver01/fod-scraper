7c2a95456ff6a7a1b875466cb9520f3b99c99a0b
electric-mike <66960109+electric-mike@users.noreply.github.com>
2025-02-16 00:39:29 -0500

    prompt

--- server/constants.ts
+++ server/constants.ts
@@ -3,6 +3,7 @@ export const prompt = `
   You are given a list of products, their stock status, and their location.
   The goal of our insights are to help inform the user on where to put their ad dollars.
   We are taking the data I will provide you and using it to buy Meta ads.
+  Try to keep your response concise and to the point.
   Here is the data:
   {data}
 `
